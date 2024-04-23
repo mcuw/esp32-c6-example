6e42ca8f069e92cc5152e1beec0232d31e5ce04f
mcuw <112967559+mcuw@users.noreply.github.com>
2024-04-23 09:51:41 +0200

    replace adafruit neopixel library with esp32 builtin hal; deactivate serial prints as default and workaround

---
+++ include/DebugUtils.h
@@ -0,0 +1,24 @@
+#ifndef DEBUG_UTILS_H
+
+/**
+ * Currently facing an issue that
+ * when using serial prints over the USB Serial
+ * the ESP32 is not working w/o an open USB Serial terminal/ monitor
+ * activate serial in platform.ini if needed:
+
+[env]
+build_flags =
+  '-DSERIAL_DEBUG'
+*/
+
+#ifdef SERIAL_DEBUG
+#define serialBegin(x) Serial.begin(x)
+#define serialPrint(x) Serial.print(x)
+#define serialPrintLn(x) Serial.println(x)
+#else
+#define serialBegin(x)
+#define serialPrint(x)
+#define serialPrintLn(x)
+#endif
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,22 @@
 #include <Arduino.h>
-#include <Adafruit_NeoPixel.h>
+
+// pinouts: https://github.com/espressif/arduino-esp32/blob/master/variants/esp32c6/pins_arduino.h
 
 // see https://github.com/wuxx/nanoESP32-C6
-// datasheet https://github.com/wuxx/nanoESP32-C6/blob/master/hardware/nanoESP32C6.pdf
-
-#define DATA_PIN 8
-#define NUMPIXELS 1
-#define BRIGHTNESS 255
-
-Adafruit_NeoPixel pixels(NUMPIXELS, DATA_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
   Serial.begin(115200);
   while (!Serial)
     ;
-
-  pixels.begin();
-  pixels.show();
-  pixels.setBrightness(BRIGHTNESS);
 }
 
 void loop()
 {
-  pixels.clear();
+  // The ESP32-C6 has an attenuator on the ADC, which defaults to 11dB. This causes the resolution to be reduced
+  analogSetAttenuation(ADC_0db);
 
-  // pixels.setPixelColor(0, pixels.Color(255, 0, 0));
-  // pixels.show(); // Send the updated pixel colors to the hardware.
+  // // blink
+  // neopixelWrite(PIN_NEOPIXEL, 0, 255, 0);
+  // delay(1000);
 }
