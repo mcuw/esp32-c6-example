f32ed424dd2627c8c02420672ae5fb6063285a6e
mcuw <112967559+mcuw@users.noreply.github.com>
2024-04-23 09:51:41 +0200

    replace adafruit neopixel library with esp32 builtin hal; deactivate serial prints as default and workaround

--- examples/rgb/main.cpp
+++ examples/rgb/main.cpp
@@ -1,42 +1,26 @@
 #include <Arduino.h>
-#include <Adafruit_NeoPixel.h>
+#include <DebugUtils.h>
+
+// pinouts: https://github.com/espressif/arduino-esp32/blob/master/variants/esp32c6/pins_arduino.h
 
 // see https://github.com/wuxx/nanoESP32-C6
-// datasheet https://github.com/wuxx/nanoESP32-C6/blob/master/hardware/nanoESP32C6.pdf
-
-#define DATA_PIN 8
-#define NUMPIXELS 1
-#define BRIGHTNESS 255
-
-Adafruit_NeoPixel pixels(NUMPIXELS, DATA_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
-  Serial.begin(115200);
-  while (!Serial)
-    ;
-
-  pixels.begin();
-  pixels.show();
-  pixels.setBrightness(BRIGHTNESS);
+  serialBegin(115200);
 }
 
 void loop()
 {
-  pixels.clear();
-
-  Serial.println("Rot");
-  pixels.setPixelColor(0, pixels.Color(255, 0, 0));
-  pixels.show(); // Send the updated pixel colors to the hardware.
+  serialPrintLn("Rot");
+  neopixelWrite(PIN_NEOPIXEL, 255, 0, 0);
   delay(1000);
 
-  Serial.println("Grün");
-  pixels.setPixelColor(0, pixels.Color(0, 255, 0));
-  pixels.show();
+  // serialPrintLn("Grün");
+  neopixelWrite(PIN_NEOPIXEL, 0, 255, 0);
   delay(1000);
 
-  Serial.println("Blau");
-  pixels.setPixelColor(0, pixels.Color(0, 0, 255));
-  pixels.show();
+  // serialPrintLn("Blau");
+  neopixelWrite(PIN_NEOPIXEL, 0, 0, 255);
   delay(1000);
 }
